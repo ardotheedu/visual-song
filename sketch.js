37da2e224c99d50f8893572201e8eba21b348bb7
eduardo.sa.lima <eduardo.lima@sysvale.com>
2023-06-17 11:24:41 -0300

    feat: cria onda a partir de musica

---
+++ sketch.js
@@ -0,0 +1,39 @@
+var song 
+var fft
+
+function preload() {  
+  song = loadSound('mozart.mp3');
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  fft = new p5.FFT();
+}
+
+function draw() {
+  background(0);
+  stroke(255);
+  noFill();
+
+  var wave = fft.waveform();
+
+  beginShape();  
+  for (var i = 0; i < wave.length; i++) {
+    var index = floor(map(i, 0, width, 0, wave.length));
+
+    var x = i
+    var y =  wave[index] * 300 + height/2
+    vertex(x, y);
+  }
+  endShape();
+}
+
+function mousePressed() {
+  if (song.isPlaying()) {
+    song.pause();
+    noLoop();
+  } else {
+    song.play();
+    loop();
+  }
+}
